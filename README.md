c2179d9fc30049580222d124304ca6ddee486289
Patrick Bennett <pbennettwa@gmail.com>
2020-01-18 10:53:08 -0800

    delete unused files from open source code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# HackerHome Photo Module
+# SpareBnB Photo Module
 
-> Photo Gallery and Carousel Module for HackerHome, a short-term vacation rental
+> Photo Gallery and Carousel Module for SpareBnB, a home rental app
 
 
 ## Table of Contents
@@ -11,50 +11,23 @@
 ## Usage
 
 ```bash
-# clone this repository
-$ git clone https://github.com/hacker-home/Photo-Module.git
-
 # Install dependencies
 $ npm install
 
-# Create CSV file to insert into SQL database
+# Create CSV file to insert into PostgresSQL
 $ npm run seedFile
 
 # Bundle files with webpack
-$ npm run react-dev
+$ npm run build
 
 # Run the app
 $ npm run start
 ```
+
+> Copy CSV file into into PostgresSQL
 
 ## Requirements
 
 - [npm](http://npmjs.com)
 - [Node.js](https://nodejs.org/en/download/)
 - [Git](https://git-scm.com)
-
-### Installing Dependencies
-
-From within the root directory:
-
-```sh
-npm install
-```
-
-### SDC CRUD ROUTES:
-
-#### GET ('/listings/:id')
-
-> express route which serves up one listing info object from the db with the id parameter
-
-#### POST ('/listings')
-
-> express route which inserts a listing info object into the db
-
-#### PUT ('/listings/:id')
-
-> express route which updates a listing info object in the db with the id parameter
-
-#### DELETE ('/listings/:id')
-
-> express route which deletes a listing info object in the db with the id parameter
--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const postgres = require('../database/postgresIndex.js');
-// const cassandraDb = require('../database/cassandraIndex.js');
-// const mongooseDb = require('../database/index.js');
+
 
 const app = express();
 
@@ -35,49 +34,5 @@ app.post('/photos', (req, res) => {
     .then(res.status(201).send())
     .catch(err => res.status(404).send(err));
 });
-
-// MongoDb
-// app.get('/photos/:listingID', (req, res) => {
-//   const targetID = req.params.listingID;
-//   mongooseDb.getPhotos(targetID, (err, photos) => {
-//     if (err) {
-//       res.status(404).send();
-//     } else {
-//       res.status(200).send(photos);
-//     }
-//   });
-// });
-
-// app.post('/photos', (req, res) => {
-//   mongooseDb.getPhotos(req.body, (err) => {
-//     if (err) {
-//       res.status(500).send();
-//     } else {
-//       res.status(201).send();
-//     }
-//   });
-// });
-
-// app.put('/photos/:listingID', (req, res) => {
-//   const targetID = req.params.listingID;
-//   mongooseDb.getPhotos(targetID, req.body, (err) => {
-//     if (err) {
-//       res.status(500).send();
-//     } else {
-//       res.status(201).send();
-//     }
-//   });
-// });
-
-// app.delete('/photos/:listingID', (req, res) => {
-//   const targetID = req.params.listingID;
-//   mongooseDb.getPhotos(targetID, (err) => {
-//     if (err) {
-//       res.status(500).send();
-//     } else {
-//       res.status(201);
-//     }
-//   });
-// });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
